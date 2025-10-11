f7538b0313cda3f3ab9c281167330cd1ac84f095
Cartridge <cartridgeauth@gmail.com>
2025-10-11 18:03:25 +0000

    nope. how is the size of the author login and book now buttons in side m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased bg-background">
         <PageLoader />
-        <div className="flex flex-col min-h-screen overflow-x-hidden">
+        <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow overflow-x-hidden">{children}</main>
           <Footer />
         </div>
         <Toaster />
