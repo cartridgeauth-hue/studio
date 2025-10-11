47d6aedecf9c207693d05a7bb2471c9009c13cee
Cartridge <cartridgeauth@gmail.com>
2025-10-11 17:05:34 +0000

    only typing works. none of the interface buttons work. ditch this librar

--- src/components/blog/RichTextEditor.tsx
+++ src/components/blog/RichTextEditor.tsx
@@ -1,93 +1,43 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import dynamic from 'next/dynamic';
-import { EditorState, ContentState, convertToHTML, convertFromHTML } from 'draft-js';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import type { EditorProps } from 'react-draft-wysiwyg';
-
-// Dynamically import the editor to avoid SSR issues
-const Editor = dynamic<EditorProps>(
-  () => import('react-draft-wysiwyg').then(mod => mod.Editor),
-  { ssr: false }
-);
+import 'react-quill/dist/quill.snow.css';
 
 interface RichTextEditorProps {
-    value: string;
-    onChange: (value: string) => void;
+  value: string;
+  onChange: (value: string) => void;
 }
 
 export default function RichTextEditor({ value, onChange }: RichTextEditorProps) {
-    const [editorState, setEditorState] = useState<EditorState | undefined>(undefined);
-    const [isClient, setIsClient] = useState(false);
+  const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
 
-    useEffect(() => {
-        // This ensures the component only renders on the client
-        setIsClient(true);
-    }, []);
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image', 'video'],
+      ['clean'],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['table'],
+    ],
+  };
 
-    useEffect(() => {
-        if (isClient) {
-            // Initialize editorState only when on the client and it hasn't been set
-            if (!editorState) {
-                if (value) {
-                    const blocksFromHTML = convertFromHTML(value);
-                    if (blocksFromHTML.contentBlocks) {
-                        const contentState = ContentState.createFromBlockArray(
-                            blocksFromHTML.contentBlocks,
-                            blocksFromHTML.entityMap
-                        );
-                        setEditorState(EditorState.createWithContent(contentState));
-                    } else {
-                        setEditorState(EditorState.createEmpty());
-                    }
-                } else {
-                    setEditorState(EditorState.createEmpty());
-                }
-            }
-        }
-    }, [isClient]);
+  if (!ReactQuill) {
+    return <div className="bg-card rounded-md border border-input min-h-[400px] animate-pulse" />;
+  }
 
-    const handleEditorChange = (state: EditorState) => {
-        setEditorState(state);
-        const html = convertToHTML(state.getCurrentContent());
-        onChange(html);
-    };
-    
-    // Add table to toolbar options
-    const toolbarOptions = {
-        options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
-        inline: {
-            options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace', 'superscript', 'subscript'],
-        },
-        blockType: {
-            options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
-        },
-        list: {
-            options: ['unordered', 'ordered', 'indent', 'outdent'],
-        },
-        textAlign: {
-            options: ['left', 'center', 'right', 'justify'],
-        },
-        image: {
-            uploadEnabled: true,
-            alt: { present: true, mandatory: false },
-        },
-    };
-
-    if (!isClient || editorState === undefined) {
-        return <div className="bg-card rounded-md border border-input min-h-[400px] animate-pulse" />;
-    }
-
-    return (
-        <div className="bg-card rounded-md border border-input">
-            <Editor
-                editorState={editorState}
-                onEditorStateChange={handleEditorChange}
-                toolbarClassName="toolbar-class !border-0 !border-b !border-input !rounded-t-md"
-                wrapperClassName="wrapper-class"
-                editorClassName="editor-class !px-4 !min-h-[300px]"
-                toolbar={toolbarOptions}
-            />
-        </div>
-    );
+  return (
+      <div className="bg-card text-foreground">
+        <ReactQuill 
+            theme="snow" 
+            value={value} 
+            onChange={onChange} 
+            modules={modules}
+            className='[&_.ql-editor]:min-h-[300px]'
+        />
+      </div>
+  );
 }
